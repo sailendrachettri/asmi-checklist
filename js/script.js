66445ee7f4d226afb83a9c4d50a5c37c8b11544c
sailendrachettri <sailendra9083@gmail.com>
2023-07-30 13:41:06 +0530

    version 1 is ready to lunch

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const inputBox = document.getElementById('input-box')
+const listContainer = document.getElementById('list-container')
+
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something")
+    }
+    else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        listContainer.appendChild(li)
+
+        // create X icon
+        let span = document.createElement('span')
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputBox.value = ""
+
+    saveData()
+}
+
+// identify entered key press and add task to task list bar
+inputBox.addEventListener('keydown', function (e) {
+    if (e.key === "Enter") {
+        addTask()
+    }
+})
+
+// click function
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked')
+        saveData()
+    }
+    else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false)
+
+// saving the data to local Storage
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML)
+}
+
+
+// showing the saved data
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data')
+}
+showTask()
